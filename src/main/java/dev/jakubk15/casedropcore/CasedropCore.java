fc510bb01b5c9eedda68bdf059dc39b098d45d42
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2021-11-26 17:09:58 +0100

    Add CommandFactory.java and cleanup CasedropCore.java

--- src/main/java/dev/jakubk15/casedropcore/CasedropCore.java
+++ src/main/java/dev/jakubk15/casedropcore/CasedropCore.java
@@ -1,11 +1,11 @@
 package dev.jakubk15.casedropcore;
 
-import dev.jakubk15.casedropcore.cmds.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CasedropCore extends JavaPlugin {
 
 	private static CasedropCore instance;
+	private CommandFactory commandFactory;
 
 	@Override
 	public void onEnable() {
@@ -16,28 +16,7 @@ public class CasedropCore extends JavaPlugin {
 		getLogger().info("/ /___  / /_/ /_(__  )/  __/  /_/ /_  /   / /_/ /_  /_/ //_____/ /___  / /_/ /  /   /  __/");
 		getLogger().info("\\____/  \\__,_/ /____/ \\___//_____/ /_/    \\____/_  .___/       \\____/  \\____//_/    \\___/ ");
 		getLogger().info("                                                /_/                                       ");
-		getCommand("feed").setExecutor(new FeedCommand());
-		getCommand("heal").setExecutor(new HealCommand());
-		getCommand("fly").setExecutor(new FlyCommand());
-		getCommand("ip").setExecutor(new IPCommand());
-		getCommand("nick").setExecutor(new NickCommand());
-		getCommand("gm").setExecutor(new GamemodeCommand());
-		getCommand("ping").setExecutor(new PingCommand());
-		getCommand("vanish").setExecutor(new VanishCommand());
-		getCommand("kick").setExecutor(new KickCommand());
-		getCommand("ac").setExecutor(new AdminChatCommand());
-		getCommand("ban").setExecutor(new BanCommand());
-		getCommand("banip").setExecutor(new BanIPCommand());
-		getCommand("chat").setExecutor(new ChatManager());
-		getCommand("helpop").setExecutor(new HelpopCommand());
-		getCommand("uuid").setExecutor(new UUIDCommand());
-		getCommand("god").setExecutor(new GodCommand());
-		getCommand("alert").setExecutor(new AlertCommand());
-		getCommand("freeze").setExecutor(new FreezeCommand());
-		getCommand("reload").setExecutor(new ReloadCommand());
-		getCommand("msg").setExecutor(new MsgCommand());
-		getCommand("mute").setExecutor(new MuteCommand());
-		getCommand("unban").setExecutor(new UnbanCommand());
+		commandFactory.registerCommands();
 		this.getConfig().options().copyDefaults(true);
 		saveConfig();
 	}
---
+++ src/main/java/dev/jakubk15/casedropcore/CommandFactory.java
@@ -0,0 +1,31 @@
+package dev.jakubk15.casedropcore;
+
+import dev.jakubk15.casedropcore.cmds.*;
+
+public class CommandFactory {
+
+	public void registerCommands() {
+		CasedropCore.getInstance().getCommand("feed").setExecutor(new FeedCommand());
+		CasedropCore.getInstance().getCommand("heal").setExecutor(new HealCommand());
+		CasedropCore.getInstance().getCommand("fly").setExecutor(new FlyCommand());
+		CasedropCore.getInstance().getCommand("ip").setExecutor(new IPCommand());
+		CasedropCore.getInstance().getCommand("nick").setExecutor(new NickCommand());
+		CasedropCore.getInstance().getCommand("gm").setExecutor(new GamemodeCommand());
+		CasedropCore.getInstance().getCommand("ping").setExecutor(new PingCommand());
+		CasedropCore.getInstance().getCommand("vanish").setExecutor(new VanishCommand());
+		CasedropCore.getInstance().getCommand("kick").setExecutor(new KickCommand());
+		CasedropCore.getInstance().getCommand("ac").setExecutor(new AdminChatCommand());
+		CasedropCore.getInstance().getCommand("ban").setExecutor(new BanCommand());
+		CasedropCore.getInstance().getCommand("banip").setExecutor(new BanIPCommand());
+		CasedropCore.getInstance().getCommand("chat").setExecutor(new ChatManager());
+		CasedropCore.getInstance().getCommand("helpop").setExecutor(new HelpopCommand());
+		CasedropCore.getInstance().getCommand("uuid").setExecutor(new UUIDCommand());
+		CasedropCore.getInstance().getCommand("god").setExecutor(new GodCommand());
+		CasedropCore.getInstance().getCommand("alert").setExecutor(new AlertCommand());
+		CasedropCore.getInstance().getCommand("freeze").setExecutor(new FreezeCommand());
+		CasedropCore.getInstance().getCommand("reload").setExecutor(new ReloadCommand());
+		CasedropCore.getInstance().getCommand("msg").setExecutor(new MsgCommand());
+		CasedropCore.getInstance().getCommand("mute").setExecutor(new MuteCommand());
+		CasedropCore.getInstance().getCommand("unban").setExecutor(new UnbanCommand());
+	}
+}
